94733d1fa85a1eab4f10267456b9f7a99c9658cf
tester-cashify <tester@cashify.in>
2021-05-05 13:31:13 +0530

    Initial commit

---
+++ src/in/cashify/Animals.java
@@ -0,0 +1,13 @@
+package in.cashify;
+
+public class Animals {
+    public void run() {
+        System.out.println("Dog is running");
+
+    }
+    public static void main(String[] args) {
+        Animals a=new Animals();
+        a.run();
+        //System.out.println("Hello");
+    }
+}
---
+++ src/in/cashify/Inheritance.java
@@ -0,0 +1,15 @@
+package in.cashify;
+
+public class Inheritance extends Animals {
+    public void eat(){
+        System.out.println("Dog also eating");
+    }
+    public static void main(String[] agrs){
+        Inheritance I= new Inheritance();
+        I.run();
+        I.eat();
+    }
+
+}
+
+
