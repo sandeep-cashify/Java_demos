e7bd4f3080806d62addfb878ef92c03b031d871a
tester-cashify <tester@cashify.in>
2021-05-05 13:55:15 +0530

    Minor changes

--- src/in/cashify/Animals.java
+++ src/in/cashify/Animals.java
@@ -5,9 +5,12 @@ public class Animals {
         System.out.println("Dog is running");
 
     }
+    Animals(){
+        System.out.println("Also sleeping");
+    }
     public static void main(String[] args) {
         Animals a=new Animals();
-        a.run();
+        //a.run();
         //System.out.println("Hello");
     }
 }
--- src/in/cashify/Inheritance.java
+++ src/in/cashify/Inheritance.java
@@ -10,6 +10,6 @@ public class Inheritance extends Animals {
         I.eat();
     }
 
-}dfdsd
+}
 
 
