e90f59ee377256552f1bd141102481097b508cfb
tester-cashify <tester@cashify.in>
2021-05-12 17:55:22 +0530

    Switch

---
+++ src/in.cashify/Encapsulation/SwitchDemo.java
@@ -0,0 +1,20 @@
+package in.cashify.Encapsulation;
+
+public class SwitchDemo {
+    public static void main(String[] args) {
+        int num=10;
+        switch (num){
+            case 10:
+                System.out.println("number 10");
+                break;
+            case  20:
+                System.out.println("number 20");
+                break;
+            case 30:
+                System.out.println("number 30");
+                break;
+            default:
+                System.out.println("number not in range");
+        }
+    }
+}
